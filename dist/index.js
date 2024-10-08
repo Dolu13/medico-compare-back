f4c5199914099025e7318b087c2188a2e4f2fefc
Dolu13 <74305815+Dolu13@users.noreply.github.com>
2024-10-08 09:12:33 +0200

    init prisma

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-// configures dotenv to work in your application
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT;
@@ -15,6 +14,5 @@ app.get("/", (request, response) => {
 app.listen(PORT, () => {
     console.log("Server running at PORT: ", PORT);
 }).on("error", (error) => {
-    // gracefully handle error
     throw new Error(error.message);
 });
